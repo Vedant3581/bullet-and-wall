2018f4cb374a37ae3c637e6f8c5cf1a425899a8d
Vedant3581 <66295277+Vedant3581@users.noreply.github.com>
2020-06-09 16:09:23 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
-var car, wall;
-var speed, weigth;
+var wall,thickness;
+var bullet,speed, weigth;
 
 function setup() {
   createCanvas(1600, 400);
@@ -8,35 +8,35 @@ function setup() {
   weigth = random(400, 1500);
 
   wall = createSprite(1000, 200, 60, height / 2);
-  car = createSprite(50, 200, 50, 50);
+  bullet = createSprite(50, 200, 50, 50);
 
-  car.velocityX = speed;
+  bullet.velocityX = speed;
 
 }
 
 function draw() {
   background(0);
 
-  if(isTouching(car,wall)){
-    car.velocityX=0;
+  if(isTouching(bullet,wall)){
+    bullet.velocityX=0;
     
-    var damage = (weigth*speed*speed)/(wall*wall*wall)
-    if(damage=3.68){
+    var damage = (0.5*weigth*speed*speed)/(wall*wall*wall)
+    if(damage<10){
       wall.shapeColor="green"
     } 
     
-    if(damage=12.43){
+    if(damage >= 10){
       wall.shapeColor="red"
     }
 
     var deformation = (0.5*weigth*speed*speed)/22500;
 
     if(deformation<100){
-      car.shapeColor="green";
+      bullet.shapeColor="green";
     }else if(deformation >= 100 && deformation < 180){
-      car.shapeColor="yellow"
+      bullet.shapeColor="yellow"
     }else if (deformation >= 180){
-      car.shapeColor="red"
+      bullet.shapeColor="red"
     }
      
   }
